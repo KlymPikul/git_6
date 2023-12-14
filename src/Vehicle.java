7b9b35ca4ec2c9d202163f19a9564ee4b65e1640
KlymPikul <klimpikul1989@gmail.com>
2023-12-14 17:01:24 +0100

    add class Vehicle

---
+++ src/Vehicle.java
@@ -0,0 +1,10 @@
+public class Vehicle {
+    private String brand;
+    private int year;
+
+    public Vehicle(String brand, int year) {
+        this.brand = brand;
+        this.year = year;
+    }
+
+}
